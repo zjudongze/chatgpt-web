845497a834a0eba658ad04a6384e2ff43173c8ab
derek.dong <derek.dong@juphoon.com>
2023-09-18 16:21:33 +0800

    修改默认用户名

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://www.gravatar.com/avatar/13040f1d38cd5fb52b812af3235bea2c?s=80&d=identicon',
+      name: 'Derek.Dong',
+      description: 'chatgpt',
     },
   }
 }
